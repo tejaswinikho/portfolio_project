11f6cc5e00074a877879cd4f77075fdabeabd834
Tejashwini khot <147027961+tejaswinikho@users.noreply.github.com>
2024-09-12 20:04:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function hamburg() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(0px)";
+}
+
+function cancel() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(-500px)";
+}
+
+// Typewriter effect
+const texts = ["DEVELOPER", "DESIGNER", "CREATOR"];
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charIndex = 0;
+
+function typeWriter() {
+    if (charIndex < texts[textIndex].length) {
+        textElements.innerHTML += texts[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, 1000);
+    }
+}
+
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50);
+    } else {
+        textIndex = (textIndex + 1) % texts.length;
+        charIndex = 0;
+        setTimeout(typeWriter, 500);
+    }
+}
+
+window.onload = typeWriter;
+
+AOS.init({ offset: 0 });
